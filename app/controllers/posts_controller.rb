f9bf6bb923858d681ebda5d1b413b94f59a7d0d1
Abeilles <afrojack.dj0628@gmail.com>
2020-10-13 19:09:41 +0900

    [Add]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to post_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    book.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
